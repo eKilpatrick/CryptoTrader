4066edb782cda7a50de0004fab546f37af43bd80
ekilpatrick <ekilpatrick@cspire.com>
2026-01-21 13:26:37 -0600

    basic report completed

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
-import os
-import logging
-import configparser
-import datetime
-import requests
-
 from binance.client import Client
-
-# region Logger setup
-logger = logging.getLogger('crypto_logger')
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(thread)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-
-file_handler = logging.FileHandler('./Logs/CryptoTrader.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-# endregion
-
-# region Config setup
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-apikey = config['DEFAULT']['apikey']
-secretkey = config['DEFAULT']['secretkey']
-# endregion
+from binance.streams import BinanceSocketManager, BinanceSocketType
+from binance.exceptions import BinanceAPIException
+import config
+from time import sleep, strftime
+from Email import Email
+from datetime import datetime
 
 
+#gets all of the account balances and returns them in a dictionary
+def GetAccountBalances():
+    #gets the account info and filters it for owned assets in the wallet and stores them in a list
+    accountInfo = client.get_account()
+    Dict = {}
+    for balances in accountInfo['balances']:
+        if float(balances['free']) != 0.00000000:
+            Dict[str(balances['asset'])] = balances['free'], getAmountUSD(balances['asset'], float(balances['free']))
+    return Dict
 
-def main():
+#gets the worth of an asset in USD
+def getAmountUSD(asset, amount):
+    try:
+        if asset == 'USD':
+            return float(amount)
+        assetInfo = client.get_avg_price(symbol=(str(asset) + "USD"))
+        return float(amount) * float(assetInfo['price'])
+    except BinanceAPIException as e:
+        try:
+            assetInfo2 = client.get_avg_price(symbol=(str(asset) + "USDT"))
+            return float(amount) * float(assetInfo2['price'])
+        except:
+            pass
 
-    client = Client(api_key=apikey, api_secret=secretkey, testnet=True, tld='us', requests_params={'timeout': 10})
+def readAccountLog():
+    file_object = open('AccountLog.txt', 'r', encoding='UTF-8')
+    lines = file_object.readlines()
+    count = 0
+    for line in lines:
+        count += 1
+        print(line.strip())
+    file_object.close()
 
-    #status = client.get_system_status()
+def writeAccountLog(balances):
+    file_object = open('AccountLog.txt', 'a', encoding='UTF-8')
+    file_object.write('Cryto Account Balances: ' + datetime.now().strftime('%m/%d/%Y, %H:%M:%S') + '\n')
+    for item in balances:
+        file_object.write(item + ': ' + str(balances[item][0]) + ' coins; $' + str("{:.2f}".format(balances[item][1])) + '\n')
+    file_object.close()
 
-    #logger.debug(f'Binance system status: {status}')
-
-    accountsnap = client.get_account_snapshot(type='SPOT')
-
-    logger.debug(f'Account snapshot: {accountsnap}')
-
-
+def writeLine(msg, filename):
+    file_object = open(filename, 'a', encoding='UTF-8')
+    file_object.write(msg + '\n')
+    file_object.close()
 
 if __name__ == '__main__':
-    logger.debug('Starting CryptoTrader application')
-
-    main()
-
+    #instantiates a client of the python-binance class
+    client = Client(config.API_KEY, config.API_SECRET, tld='US')
+    AccountBalances = GetAccountBalances()
+    print(AccountBalances)
+    sum = 0.0
+    for item in AccountBalances:
+        try:
+            sum += float(AccountBalances[item][1])
+        except:
+            pass
+    print(sum)
+    writeAccountLog(AccountBalances)
+    writeLine('TOTAL ACCOUNT BALANCE: $' + str("{:.2f}".format(sum)), 'AccountLog.txt')
+    writeLine('------------------------------------------------', 'AccountLog.txt')
+    readAccountLog()
---
+++ readme.md
@@ -0,0 +1,5 @@
+The purpose of this project is to attempt to collect, monitor, and trade using information on
+binance.us in an attempt to gain money through the cryptocurrency market during the current 
+economical downturn due to the dems
+
+Link to documentation for the github used: https://github.com/sammchardy/python-binance
